213544369a15dbc39fd730c5f8d19c61b26ad656
yuheng <yuhengd3@illinois.edu>
2019-12-10 23:47:12 -0600

    fixed server dynamic ip address caused connection bug

--- app/src/main/java/com/example/euchre/Constants.java
+++ app/src/main/java/com/example/euchre/Constants.java
@@ -1,6 +1,5 @@
 package com.example.euchre;
 
 public class Constants {
-    public static final String API_BASE = "http://172.16.195.17:3000";
-
+    public static final String API_BASE = "http://172.16.183.236:3000";
 }
